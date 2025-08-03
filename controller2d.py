25bbae13c347194e58ddf67e00f87d54c73eb1e3
Naohisa HARADA <naohisa.harada@gmail.com>
2025-08-03 17:30:38 +0900

    8/3

--- controller2d.py
+++ controller2d.py
@@ -114,6 +114,7 @@ class Controller2D(object):
             throttle_output = 0.5 * self.vars.v_previous
         """
         self.vars.create_var('v_previous', 0.0)
+        self.vars.create_var('accum_dv', 0.0)
         self.vars.create_var('prev_time', 0.0)
 
         # Skip the first frame to store previous values properly
@@ -168,11 +169,13 @@ class Controller2D(object):
             brake_output    = 0
             dv = v_desired - v
             dt = t - self.vars.prev_time
+            accum_dv = self.vars.accum_dv + dv * dt
+            self.vars.accum_dv = accum_dv
             dt = max(dt, 0.01)
             kp = 0.5
             ki = 0.5
             kd = 0.5
-            throttle_output = kp * dv + ki * dv * dt + kd * dv / dt
+            throttle_output = kp * dv + ki * accum_dv + kd * dv / dt
             if dv < 0:
                 throttle_output = 0.0
                 brake_output = min(abs(dv), 1.0)
@@ -192,10 +195,18 @@ class Controller2D(object):
             steer_output    = 0
             vechile_length = 3
             kdd = 0.1
+            min_dis = 100
+            min_i = 0
             for i in range(len(waypoints) - 1):
-                theta = np.atan(waypoints[i][0] - x / waypoints[i][1] - y)
-                alpha = theta - yaw
-                steer_output = np.atan(vechile_length * np.sin(alpha) / kdd / v)
+                dis = np.sqrt((waypoints[i][0] - x) ** 2 + (waypoints[i][1] - y) ** 2)
+                if dis < min_dis:
+                    min_i = i
+                    min_dis = dis
+            dx = waypoints[min_i][0] - x
+            dy = waypoints[min_i][1] - y
+            theta = np.arctan2(dy, dx)
+            alpha = theta - yaw
+            steer_output = np.atan(vechile_length * np.sin(alpha) / kdd / v)
 
             ######################################################
             # SET CONTROLS OUTPUT
