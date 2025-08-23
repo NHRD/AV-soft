69387b695e3e1d2f65236976e369496b4b74a4f6
NHRD <naohisa.harada@gmail.com>
2025-08-24 01:53:51 +0900

    8/24

---
+++ Enhanced_Kalman_Filter.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+dt = 0.5
+u0 = -2
+y1 = np.pi/6  # 30度
+S = 20
+D = 40
+x0 = np.array([[0], [5]])
+p0 = x0[0, 0]
+
+Fk = np.array([[1, dt], [0, 1]])
+Lk = np.eye(2)
+Hk = np.array([S/(D - p0)**2, 0])
+M = np.eye(2)
