2b30d43c3c5c427f69217ec5ce1cf524e895eb14
NHRD <naohisa.harada@gmail.com>
2025-08-24 02:26:43 +0900

    8/24 3

--- Enhanced_Kalman_Filter.py
+++ Enhanced_Kalman_Filter.py
@@ -6,10 +6,22 @@ y1 = np.pi/6  # 30度
 S = 20
 D = 40
 x0 = np.array([[0], [5]])
-p0 = x0[0, 0]
-
+P = np.array([[0.01, 0], [0, 1]])
 Fk = np.array([[1, dt], [0, 1]])
 Lk = np.eye(2)
-Hk = np.array([[S/((D - p0)**2 + S**2), 0]])
-M = np.array([[1]])
-P = np.array([[0.01, 0], [0, 1]])
+Gk = np.array([[0], [dt]])
+Qk = np.array([[0.01, 0], [0, 0.01]])
+Rk = np.array([[0.01]])
+
+x_check = Fk @ x0 + Gk * u0
+p_check = x_check[0, 0]
+Hk = np.array([[S/((D - p_check)**2 + S**2), 0]])
+Mk = np.array([[1]])
+y_check = np.arctan(S / (D - p_check))   # スカラー
+y_check = y1 - y_check
+P_check = Fk @ P @ Fk.T + Lk @ Qk @ Lk.T
+Kk = P_check @ Hk.T @ np.linalg.inv(Hk @ P_check @ Hk.T + Mk @ Rk @ Mk.T)
+x_est = x_check + Kk @ (y_check - np.arctan(S / (D - p_check)))
+P_est = (np.eye(2) - Kk @ Hk) @ P_check
+
+print(x_est, P_est)
